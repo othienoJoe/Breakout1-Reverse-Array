fdf0692b989975ce4b4ca8f6ad8f97f8f3fde552
othieno <nicothieno@gmail.com>
2021-07-15 10:46:21 +0300

    added js and README

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
 # Breakout1-Reverse-Array
+# Developed By :
+
+## Nicodemus
+## Joseph
+## Samuel
+## Marial
+## Bernard 
+## Nancy
---
+++ scripts.js
@@ -0,0 +1,12 @@
+// Assignments
+// 1. Reverse Array In-place
+// Write a function reverseArray(A) that takes in an array A and reverses it, without using another array or collection data structure; in-place.
+
+// Example:
+
+A = [10,5,6,9] //[9,6,5,10]
+A.reverse();
+console.log(A);
+
+//Output 
+//[9, 6, 5, 10]
